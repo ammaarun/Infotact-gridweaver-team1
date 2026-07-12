c5c6d64f33c902157f479c22b019b35600c652c3
ammaarun <ammaarunkumar2003@gmail.com>
2026-07-13 00:56:10 +0530

    Set up SpringBoot 3.2 backend skeleton with virtual threads enabled

---
+++ backend/src/main/java/com/infotact/gridweaver/GridWeaverApplication.java
@@ -0,0 +1,11 @@
+package com.infotact.gridweaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridWeaverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GridWeaverApplication.class, args);
+    }
+}
